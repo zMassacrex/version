15c06c896eb6ce2d461641edd82c79e41eb1ea3c
zMaSs4cR3x <106979384+zMassacrex@users.noreply.github.com>
2024-10-12 14:59:15 -0400

    Update version-purge.lua

--- version-purge.lua
+++ version-purge.lua
@@ -1,7 +1,7 @@
 {
     "version": "1.4",
     "changeLogs": 
-    " + Compatible with origen inventory"
+    " + Compatible with origen inventory"..
     " + Compatible with qb inventory",
     "releaseDate": "10/12/24"
 }
